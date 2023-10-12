d4f365f01d628f6566be608c26840284141127d9
Stark Rosck <94766289+StarkRosck@users.noreply.github.com>
2023-10-12 18:03:28 -0300

    Subindo

---
+++ app/login.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+        integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+    <style>
+
+        .container {
+            max-width: 400px;
+            margin: 0 auto;
+            background-color: #222;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            margin-top: 60px;
+        }
+
+        h2 {
+            text-align: center;
+            color: #dc3545;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 5px;
+            color: #fff;
+        }
+
+        input[type="text"],
+        input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        input[type="submit"] {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #555;
+        }
+    </style>
+    <title>Cadastro</title>
+</head>
+<body>
+<header>
+        <h1>Dev <span>Arthur</span></h1>
+        <nav>
+            <ul aria-label="Navegação primária" class="navegacao-primaria">
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="area_cliente.php">Projetos</a></li>
+                <li><a href="cadastro.php">Cadastro</a></li>
+            </ul>
+            <i class="fa-solid fa-bars"></i>
+        </nav>
+    </header>
+<div class="container">
+        <h2>Login</h2>
+        <form action="sql/login.php" method="post">
+            <label for="nomeEmpresa">Gmail da Empresa:</label>
+            <input type="text" id="nomeEmpresa" name="nomeEmpresa" required>
+
+            <label for="senha">Senha:</label>
+            <input type="password" id="senha" name="senha" required>
+
+            <input type="submit" value="Logar">
+        </form>
+    </div>
+    <footer>
+        <div class="sobre_titulo">
+            <h1 id="contact">Arthur </h1>
+            <p>Follow my social networks</p>
+        </div>
+        <nav aria-label="navegação de links sociais">
+            <ul class="footer_sociais">
+                <li><i class="fa-brands fa-facebook-f"><a href="#"></a></i></li>
+                <li><i class="fa-brands fa-twitter"><a href="#"></a></i></li>
+                <a href="https://www.linkedin.com/in/arthur-dem%C3%A9trio-527050205/"><li><i class="fa-brands fa-linkedin-in"></i></li></a>
+                <a href="https://www.instagram.com/arthurstarkrosck/"><li><i class="fa-brands fa-instagram"></a></i></li></a>
+                <a href="https://github.com/StarkRosck/"><li><i class="fa-brands fa-github-alt"></a></i></li></a>
+            </ul>
+        </nav>
+        <p class="footer_comercial">Todos os direitos reservados Arthur Stark Rosck &copy; - 2023</p>
+    </footer>
+
+</body>
+</html>
